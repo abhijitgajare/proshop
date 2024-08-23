ae7c22ea3679de6c381d137ffed239bda7b29bf3
AbhijitGajare <abhijitgajare7@gmail.com>
2024-08-23 20:57:58 +0530

    add schema

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,13 @@ import express from 'express';
 // const express = require('express');
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 const port = process.env.PORT || 5000;
+
+connectDB(); // Connect to MongoDB
+
 const app = express();
 
 app.get('/', (req, res) => {
